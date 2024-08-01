19ad0ca1eb3568564cd880023cd6b95e40b87b36
zzDanDanzz <dansfancymaths@gmail.com>
2024-08-01 23:44:24 +0330

    supermaven ignore markdown

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -4,6 +4,7 @@ return {
     require("supermaven-nvim").setup({
       disable_inline_completion = true, -- disables inline completion for use with cmp
       disable_keymaps = true, -- disables built in keymaps for more manual control
+      ignore_filetypes = { md = true, markdown = true },
     })
   end,
 }
